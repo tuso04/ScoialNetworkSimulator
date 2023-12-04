6468cf8003522666090850c45fac3ba510f105aa
Tim <botknutsen@posteo.de>
2023-12-04 11:14:41 +0100

    Beginn Aufbau Grundstruktur Tim 04.12.23 11:14

---
+++ message.py
@@ -0,0 +1,8 @@
+class Message:
+
+    def __init__(self, mood):
+        self._mood = mood
+
+    def get_mood(self):
+        return self._mood
+
---
+++ network.py
@@ -0,0 +1,7 @@
+class Network:
+    def __init__(self, turbulence_factor):
+        self._turbulence_factor = turbulence_factor
+
+
+    def get_turbulence_factor(self):
+        return self._turbulence_factor
