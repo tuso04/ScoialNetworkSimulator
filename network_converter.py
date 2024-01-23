6b465f0df32b3744a5a718dcd06e5577d5604682
Tim <botknutsen@posteo.de>
2024-01-23 01:46:19 +0100

    Darstellung Graph mit Farbänderung funktioniert. Lange Ladezeiten

--- network_converter.py
+++ network_converter.py
@@ -6,9 +6,8 @@ import network
 
 def network_form_json(json_file):
     # Wandle JSON-Daten in networkx-Graph um
-    network_graph = nx.adjacency_graph(json_file)
 
     # Erstelle Netzwerk-Objekt
-    current_network = network.Network(network_graph, turbulence_factor=0.7)  # Umgang mit Turbulenz-Factor klären
+    current_network = network.Network(json_file, turbulence_factor=0.7)  # Umgang mit Turbulenz-Factor klären
     return current_network
 
