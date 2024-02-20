74332b3755bf5ecdeb3f31b7e8a1a54f8265fc3f
Tim <botknutsen@posteo.de>
2024-02-20 15:15:55 +0100

    Bug fixes Teilnehmerparameter

--- network_generator.py
+++ network_generator.py
@@ -1,10 +1,6 @@
 import random
 
 import networkx as nx
-import matplotlib.pyplot as plt
-
-import network_participant
-import network
 
 
 def generate_new_network(shape, participant_params, n_nodes=200, init_edges=2, split_prob=0.1, ):
@@ -22,18 +18,18 @@ def generate_new_network(shape, participant_params, n_nodes=200, init_edges=2, s
     nodes = network_graph.nodes()
 
     for i in range(len(nodes)):
-        threshold_belive_p = random.randint(1, 100) / 100
-        indifference = random.randint(1, 100) / 100
-        isi_parameter = random.randint(1, 100) / 100
-        fi_parameter = random.randint(1, 100) / 100
-        purchase_prob = random.randint(1, 100) / 100
-        purchase_prob_max = 0.125
-        purchase_prob_min = 0.05
-        purchase_expo_param_negative = 2.5
-        purchase_expo_param_positive = 5
+        threshold_believe_p = random.normalvariate(participant_params["threshold_believe"], 1)
+        indifference = random.normalvariate(participant_params["indifference"], 1)
+        isi_parameter = random.normalvariate(participant_params["isi_parameter"], 1)
+        fi_parameter = random.normalvariate(participant_params["fi_parameter"], 1)
+        purchase_prob = random.normalvariate(participant_params["purchase_init_prob"], 1)
+        purchase_prob_max = participant_params["purchase_prob_max"]
+        purchase_prob_min = participant_params["purchase_prob_min"]
+        purchase_expo_param_negative = participant_params["purchase_expo_param_positive"]
+        purchase_expo_param_positive = participant_params["purchase_expo_param_negative"]
 
         nodes[i]["np_id"] = i
-        nodes[i]["threshold_belive_p"] = threshold_belive_p
+        nodes[i]["threshold_believe_p"] = threshold_believe_p
         nodes[i]["turbulence_factor"] = turbulence_factor
         nodes[i]["indifference"] = indifference
         nodes[i]["isi_parameter"] = isi_parameter
@@ -50,11 +46,10 @@ def generate_new_network(shape, participant_params, n_nodes=200, init_edges=2, s
         nodes[i]["forwarding"] = False
         nodes[i]["purchase"] = False
 
-
         """
         nodes[i]["object"] = network_participant.Network_Participant(np_id=i,
                                                                      network=net,
-                                                                     threshold_belive_p=threshold_belive_p,
+                                                                     threshold_believe_p=threshold_believe_p,
                                                                      indifference=indifference,
                                                                      isi_parameter=isi_parameter,
                                                                      fi_parameter=fi_parameter,
@@ -68,7 +63,7 @@ def generate_new_network(shape, participant_params, n_nodes=200, init_edges=2, s
     nx.set_edge_attributes(network_graph, bind, "bond")
 
     for e in network_graph.edges:
-        network_graph.edges[e[0], e[1]]["bond"] = random.randint(0, 100)/100, random.randint(0, 100)/100
+        network_graph.edges[e[0], e[1]]["bond"] = random.randint(0, 100) / 100, random.randint(0, 100) / 100
 
     # Umwandlung in JSON-Format
     json_data = nx.adjacency_data(network_graph)
